ddc79c9deae0b30bafef4f854edf2e490a4f0f02
Robby Michael <Robby Michael>
2017-04-11 09:34:03 -0400

    initial commit

---
+++ MyGuessingGame/Controllers/TheGameController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyGuessingGame.Controllers
+{
+    public class TheGameController : Controller
+    {
+        // GET: TheGame
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
